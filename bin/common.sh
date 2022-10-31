49d3778c685abd08ec78be0ca86a0c5ed1400c1f
Emile Ndagijimana <emile.ndagijimana@apitech.fr>
2022-10-31 16:14:10 +0100

    adding add_templates and add_plugins function

--- bin/common.sh
+++ bin/common.sh
@@ -229,3 +229,36 @@ function fetch_provider_dist() {
   cp "${CACHE_DIR}/dist/${dist}" "${location}"
   mv "${location}/${provider_name}-${version}.jar" "${dest}/providers/${provider_name}.jar"
 }
+
+function add_templates() {
+  local keycloak_templates_dir="$1"
+  local keycloak_path="$2"
+  if [ -d "${keycloak_templates_dir}" ]; then
+    cp_keycloak_themes=$(cp -R "${keycloak_templates_dir}"/* "${keycloak_path}/themes")
+    if [ "${cp_keycloak_themes}" != "" ]; then
+      echo "copy keycloak themes: ${cp_keycloak_themes}"
+    fi
+  else
+    if [ -n "$BUILDPACK_DEBUG" ]; then
+      echo "KEYCLOAK_TEMPLATES_DIR defined and ${keycloak_templates_dir} dir does not exist!!!"
+    fi
+  fi
+}
+
+function add_plugins() {
+  local keycloak_plugins_dir="$1"
+  local keycloak_path="$2"
+  if [ -d "${keycloak_plugins_dir}" ]; then
+    cp_keycloak_plugins=$(cp -R "${keycloak_plugins_dir}"/* "${keycloak_path}/providers")
+    if [ "${cp_keycloak_plugins}" != "" ]; then
+      echo "copy keycloak plugins: ${cp_keycloak_plugins}"
+    fi
+    echo "!_! copy keycloak plugins: ${cp_keycloak_plugins}"
+    echo "!_! ls plugins: "
+    echo "$(ls ${keycloak_path}/providers)"
+  else
+    if [ -n "$BUILDPACK_DEBUG" ]; then
+      echo "KEYCLOAK_PLUGINS_DIR defined and ${keycloak_plugins_dir} dir does not exist!!!"
+    fi
+  fi
+}
